3729224620e480f7eef04c8aca577d4b84c06150
TheStaches <Eric.B.Dodds@gmail.com>
2018-08-30 13:30:50 -0700

    Changes server port

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -8,7 +8,7 @@ module.exports = {
   'MongoDB': {
     'host': 'ds237832.mlab.com',
     'port': 37832,
-    'url': '',
+    'url': process.env.MONGODB_URI,
     'database': process.env.MONGODB_USERNAME,
     'username': process.env.MONGODB_USERNAME,
     'password': process.env.MONGODB_PASSWORD,
