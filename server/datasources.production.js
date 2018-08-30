7328502aa7da1a3bf17965a4ef404c2a8264437c
TheStaches <Eric.B.Dodds@gmail.com>
2018-08-30 13:41:43 -0700

    Changes server port

---
+++ server/datasources.production.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  'db': {
+    'name': 'db',
+    'connector': 'memory',
+  },
+  'MongoDB': {
+    'host': 'ds237832.mlab.com',
+    'port': 37832,
+    'url': process.env.MONGODB_URI,
+    'database': process.env.MONGODB_USERNAME,
+    'username': process.env.MONGODB_USERNAME,
+    'password': process.env.MONGODB_PASSWORD,
+    'name': 'MongoDB',
+    'connector': 'mongodb',
+  },
+};
